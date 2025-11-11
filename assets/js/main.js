64b385fce884f2be799f9c89d6b4f5c8fd7ed7f4
vwang99 <v_wang@uncg.edu>
2025-11-11 16:09:15 -0500

    added scripts for github fetch

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,7 @@ jQuery(document).ready(function ($) {
 
 
 		/*======= Populate GitHub Projects *=======*/
-		let gitHubUsername = 'YOUR GITHUB USERNAME';
+		let gitHubUsername = 'vwang99';
 
 		// Run GitHub API function, passing in the GitHub username
 		requestUserRepos(gitHubUsername)
